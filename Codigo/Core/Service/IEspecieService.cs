7b8b5f144186db8e97444d8e941517adfa26936b
Rafael S. Lima <rafaelkra2@gmail.com>
2025-08-14 06:40:22 -0300

    add: IEspecieService para gerenciamento de EspecieDto
    Introduz a interface `IEspecieService` no namespace `Core.Service`.
    Esta interface inclui métodos para criar, editar, excluir e recuperar
    objetos `EspecieDto` por ID, todos e por nome.

---
+++ Codigo/Core/Service/IEspecieService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IEspecieService
+    {
+        uint Create(EspecieDto especie);
+        void Edit(EspecieDto especie);
+        void Delete(uint id);
+        EspecieDto? Get(uint id);
+        IEnumerable<EspecieDto> GetAll();
+        IEnumerable<EspecieDto> GetByNome(string nome);
+    }
+}
