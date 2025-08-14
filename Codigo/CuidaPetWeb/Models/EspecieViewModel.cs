744d4cab55b954f993748c6e8ce0cc92281dfe3a
Rafael S. Lima <rafaelkra2@gmail.com>
2025-08-14 06:14:54 -0300

    add: classe EspecieViewModel com propriedades Id e Nome
    Introduzida uma nova classe `EspecieViewModel` no namespace `CuidaPetWeb.Models`.
    Esta classe apresenta uma propriedade `Id` como um inteiro sem sinal obrigatório com um atributo `[Key]`,
    e uma propriedade `Nome` como uma string obrigatória com um comprimento máximo de 100 caracteres,
    utilizando os atributos `[Required]` e `[StringLength(100)]`.
    A propriedade `Nome` é inicializada como uma string não anulável.

---
+++ Codigo/CuidaPetWeb/Models/EspecieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuidaPetWeb.Models
+{
+    public class EspecieViewModel
+    {
+        [Key]
+        public uint Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; } = null!;
+    }
+}
