771ee5b2f717109f35c9d2a9b0016a1310b1ad1f
alvarostavares <sralvarotavares@gmail.com>
2025-08-18 23:08:49 -0300

    Realiza criação de CRUD para o caso de uso "Manter Doenças". Close #122

---
+++ Codigo/Core/Service/IDoencaService.cs
@@ -0,0 +1,15 @@
+﻿using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IDoencaService
+    {
+        uint Create(Doenca doenca);
+        void Edit(Doenca doenca);
+        void Delete(uint id);
+        Doenca? Get(uint id);
+        IEnumerable<Doenca> GetAll();
+        IEnumerable<DoencaDTO> GetByNome(string nome);
+
+    }
+}
---
+++ Codigo/CuidaPetWeb/Models/DoencaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuidaPetWeb.Models
+{
+    public class DoencaViewModel
+    {
+        [Required]
+        [Key]
+        public uint Id { get; set; }
+        [Required(ErrorMessage = "O nome da doença é obrigatório.")]
+        [Display(Name = "Nome da Doença")]
+
+        public string Nome { get; set; } = null!;
+        [Required(ErrorMessage = "A descrição da doença é obrigatória.")]
+        [Display(Name = "Descrição da Doença")]
+        public uint IdEspecie { get; set; }
+    }
+}
