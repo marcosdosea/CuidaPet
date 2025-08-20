7240222c2450532b1e8bd426e765239198304032
KEWEN SILVA <kewensilva58@gmail.com>
2025-08-20 18:17:55 -0300

    add dto para vacina

---
+++ Codigo/Core/DTO/VacinaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DTO;
+public class VacinaDTO
+{
+    public uint Id { get; set; }
+    public string Nome { get; set; } = null!;
+    public ushort? PeriodoEmDias { get; set; }
+    public string Doenca { get; set; } = null!;
+    public string Especie { get; set; } = null!;
+}
