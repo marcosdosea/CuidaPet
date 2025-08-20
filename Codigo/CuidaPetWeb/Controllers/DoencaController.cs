23fc3d6c76319dc7b6c253c86f81877433a14fb4
alvarostavares <sralvarotavares@gmail.com>
2025-08-20 10:34:41 -0300

    novas alterações em CRUD caso de uso Manter Doenças.

--- Codigo/CuidaPetWeb/Controllers/DoencaController.cs
+++ Codigo/CuidaPetWeb/Controllers/DoencaController.cs
@@ -84,7 +84,7 @@ namespace CuidaPetWeb.Controllers
         }
 
         // POST: DoencaController/Delete/5
-        [HttpPost
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(uint id, DoencaViewModel doencaViewModel)
         {
