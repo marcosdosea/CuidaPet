a5538c88b276f9bbd1545ba0b93920d0e89a7b0d
CleomadsonMedeiros <cleomadsonmedeiros@gmail.com>
2025-08-27 22:12:08 -0300

    add: crud de funcionários, projeto "util" com métodos de validações personalizadas e validation atrributes

---
+++ Codigo/CuidaPetWeb/Controllers/FuncionarioController.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using CuidaPetWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CuidaPetWeb.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private readonly IFuncionarioService funcionarioService;
+        private readonly IMapper mapper;
+
+        public FuncionarioController(IFuncionarioService funcionarioService, IMapper mapper)
+        {
+            this.funcionarioService = funcionarioService;
+            this.mapper = mapper;
+        }
+
+
+        // GET: FuncionarioController
+        public ActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var funcionarios = funcionarioService.GetAll(page, pageSize);
+            var funcionarioViewModel = mapper.Map<IEnumerable<FuncionarioViewModel>>(funcionarios);
+
+            ViewBag.TotalItems = funcionarioService.GetCount();
+            ViewBag.Page = page;
+            ViewBag.PageSize = pageSize;
+
+            return View(funcionarioViewModel);
+        }
+
+        // GET: FuncionarioController/Details/5
+        public ActionResult Details(uint id)
+        {
+            var funcionario = funcionarioService.Get(id);
+
+            var funcionarioViewModel = mapper.Map<FuncionarioViewModel>(funcionario);
+            return View(funcionarioViewModel);
+        }
+
+        // GET: FuncionarioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FuncionarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FuncionarioViewModel funcionarioViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var funcionario = mapper.Map<Funcionario>(funcionarioViewModel);
+                funcionarioService.Create(funcionario);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcionarioViewModel);
+        }
+
+        // GET: FuncionarioController/Edit/5
+        public ActionResult Edit(uint id)
+        {
+            var funcionario = funcionarioService.Get(id);
+            var funcionarioViewModel = mapper.Map<FuncionarioViewModel>(funcionario);
+            return View(funcionarioViewModel);
+        }
+
+        // POST: FuncionarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(FuncionarioViewModel funcionarioViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var funcionario = mapper.Map<Funcionario>(funcionarioViewModel);
+                funcionarioService.Edit(funcionario);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcionarioViewModel);
+        }
+
+        // GET: FuncionarioController/Delete/5
+        public ActionResult Delete(uint id)
+        {
+            var funcionario = funcionarioService.Get(id);
+            var funcionarioViewModel = mapper.Map<FuncionarioViewModel>(funcionario);
+            return View(funcionarioViewModel);
+        }
+
+        // POST: FuncionarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(uint id, FuncionarioViewModel funcionarioViewModel)
+        {
+            funcionarioService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Codigo/Service/FuncionarioService.cs
@@ -0,0 +1,89 @@
+﻿using Core;
+using Core.DTO;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service;
+
+public class FuncionarioService : IFuncionarioService
+{
+    private readonly CuidaPetContext context;
+
+    public FuncionarioService(CuidaPetContext context)
+    {
+        this.context = context;
+    }
+
+    /// <summary>
+    /// Criar um novo funcionário na base de dados
+    /// </summary>
+    /// <param name="funcionario">Dados do Funcionário</param>
+    /// <returns>ID do Funcionário</returns>
+    public uint Create(Funcionario funcionario)
+    {
+        context.Funcionarios.Add(funcionario);
+        context.SaveChanges();
+        return funcionario.Id;
+    }
+
+    /// <summary>
+    /// Editar um funcionário existente na base de dados
+    /// </summary>
+    /// <param name="funcionario">Dados do Funcionário</param>
+    public void Edit(Funcionario funcionario)
+    {
+        context.Funcionarios.Update(funcionario);
+        context.SaveChanges();
+    }
+
+    /// <summary>
+    /// Deletar um funcionário da base de dados
+    /// </summary>
+    /// <param name="id">ID do Funcionário</param>
+    public void Delete(uint id)
+    {
+        var funcionario = context.Funcionarios.Find(id);
+        if (funcionario != null)
+        {
+            context.Funcionarios.Remove(funcionario);
+            context.SaveChanges();
+        }
+    }
+
+    /// <summary>
+    /// Buscar um funcionário na base de dados
+    /// </summary>
+    /// <param name="id">ID do Funcionário</param>
+    /// <returns>Dados do Funcionário</returns>
+    public Funcionario? Get(uint id)
+    {
+        return context.Funcionarios
+            .Include(f => f.IdPessoaNavigation)
+            .Include(f => f.IdEstabelecimentoNavigation)
+            .FirstOrDefault(f => f.Id == id);
+    }
+
+    /// <summary>
+    /// Buscar todos os funcionários na base de dados
+    /// </summary>
+    /// <returns>Lista de Funcionários</returns>
+    public IEnumerable<Funcionario> GetAll(int page = 1, int pageSize = 10)
+    {
+        return context.Funcionarios
+                .Include(f => f.IdEstabelecimentoNavigation)
+                .Include(f => f.IdPessoaNavigation)
+                .AsNoTracking()
+                .OrderBy(f => f.Id)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize);
+    }
+
+    /// <summary>
+    /// Obter contagem total de funcionários
+    /// </summary>
+    /// <returns>Número total de funcionários</returns>
+    public int GetCount()
+    {
+        return context.Funcionarios.Count();
+    }
+}
