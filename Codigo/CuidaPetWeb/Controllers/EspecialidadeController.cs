119041e7d09af3f1860515dc31f91b4137917d0f
alvarostavares <sralvarotavares@gmail.com>
2025-09-07 19:09:30 -0300

    realiza implementação de CRUD para caso de uso Manter Especialidade

---
+++ Codigo/CuidaPetWeb/Controllers/EspecialidadeController.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using CuidaPetWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CuidaPetWeb.Controllers
+{
+    public class EspecialidadeController : Controller
+    {
+        private readonly IEspecialidadeService especialidadeService;
+        private readonly IMapper mapper;
+
+        public EspecialidadeController(IEspecialidadeService especialidadeService, IMapper mapper)
+        {
+            this.especialidadeService = especialidadeService;
+            this.mapper = mapper;
+        }
+
+        // GET: EspecialidadeController
+        public ActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var especialidades = especialidadeService.GetAll(page, pageSize);
+            var especialidadeViewModels = mapper.Map<IEnumerable<EspecialidadeViewModel>>(especialidades);
+
+            ViewBag.Page = page;
+            ViewBag.PageSize = pageSize;
+            ViewBag.TotalItems = especialidadeService.GetCount();
+
+            return View(especialidadeViewModels);
+        }
+
+        // GET: EspecialidadeController/Details/5
+        public ActionResult Details(uint id)
+        {
+            var especialidade = especialidadeService.Get(id);
+            var especialidadeViewModel = mapper.Map<EspecialidadeViewModel>(especialidade);
+            return View(especialidadeViewModel);
+        }
+
+        // GET: EspecialidadeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EspecialidadeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EspecialidadeViewModel especialidadeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var especialidade = mapper.Map<Especialidade>(especialidadeViewModel);
+                especialidadeService.Create(especialidade);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidadeViewModel);
+        }
+
+        // GET: EspecialidadeController/Edit/5
+        public ActionResult Edit(uint id)
+        {
+            var especialidade = especialidadeService.Get(id);
+            var especialidadeViewModel = mapper.Map<EspecialidadeViewModel>(especialidade);
+            return View(especialidadeViewModel);
+        }
+
+        // POST: EspecialidadeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EspecialidadeViewModel especialidadeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var especialidade = mapper.Map<Especialidade>(especialidadeViewModel);
+                especialidadeService.Edit(especialidade);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidadeViewModel);
+        }
+
+        // GET: EspecialidadeController/Delete/5
+        public ActionResult Delete(uint id)
+        {
+            var especialidade = especialidadeService.Get(id);
+            var especialidadeViewModel = mapper.Map<EspecialidadeViewModel>(especialidade);
+            return View(especialidadeViewModel);
+        }
+
+        // POST: EspecialidadeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(uint id, EspecialidadeViewModel especialidadeViewModel)
+        {
+            especialidadeService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Codigo/CuidaPetWeb/Models/EspecialidadeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuidaPetWeb.Models
+{
+    public class EspecialidadeViewModel
+    {
+        [Key]
+        public uint Id { get; set; }
+        [Display(Name = "Nome da Especialidade")]
+        
+        public string Nome { get; set; } = null!;
+        [Required(ErrorMessage = "O nome da especialidade é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
+
+        public string Descricao { get; set; } = null!;
+    }
+}
