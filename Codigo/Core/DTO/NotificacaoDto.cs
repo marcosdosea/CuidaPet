2f898865ab7b5c666e987144d352496a97744462
Rafael S. Lima <rafaelkra2@gmail.com>
2025-09-08 19:49:08 -0300

    add: Dto da entidade Notificacao

---
+++ Codigo/Core/DTO/NotificacaoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.DTO
+{
+    public class NotificacaoDto
+    {
+        public uint Id { get; set; }
+        public string Titulo { get; set; } = null!;
+        public string? Descricao { get; set; }
+        public DateTime DataEnvio { get; set; }
+        public uint IdPessoa { get; set; }
+        public bool Lida { get; set; }
+    }
+}
