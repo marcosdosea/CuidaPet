4f48b9f3d669f672064bb980864dde1085658c07
Rafael S. Lima <rafaelkra2@gmail.com>
2025-09-08 19:48:46 -0300

    add: viewModel da entidade Notificacao

---
+++ Codigo/CuidaPetWeb/Models/NotificacaoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CuidaPetWeb.Models
+{
+    public class NotificacaoViewModel
+    {
+        [Key]
+        public uint Id { get; set; }
+
+        [Required(ErrorMessage = "O título da notificação é obrigatório.")]
+        [StringLength(45)]
+        [DisplayName("Título")]
+        public string Titulo { get; set; } = null!;
+
+        [StringLength(150)]
+        [DisplayName("Descrição")]
+        public string? Descricao { get; set; }
+
+        [Required(ErrorMessage = "A data de envio da notificação é obrigatória.")]
+        [DisplayName("Data de envio da notificação")]
+        public DateTime DataEnvio { get; set; }
+    }
+}
