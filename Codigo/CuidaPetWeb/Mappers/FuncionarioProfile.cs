c7e952ea8cbef0695f1dcc3475238504670d8c07
CleomadsonMedeiros <cleomadsonmedeiros@gmail.com>
2025-09-08 23:26:45 -0300

    add: view moderna refact: auto mapper

--- Codigo/CuidaPetWeb/Mappers/FuncionarioProfile.cs
+++ Codigo/CuidaPetWeb/Mappers/FuncionarioProfile.cs
@@ -8,6 +8,23 @@ public class FuncionarioProfile : Profile
 {
     public FuncionarioProfile()
     {
-        CreateMap<Funcionario, FuncionarioViewModel>().ReverseMap();
+        CreateMap<Funcionario, FuncionarioViewModel>()
+            .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.IdPessoaNavigation.Nome))
+            .ForMember(dest => dest.Cpf, opt => opt.MapFrom(src => src.IdPessoaNavigation.Cpf))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.IdPessoaNavigation.Email))
+            .ForMember(dest => dest.Senha, opt => opt.MapFrom(src => src.IdPessoaNavigation.Senha))
+            .ForMember(dest => dest.Telefone, opt => opt.MapFrom(src => src.IdPessoaNavigation.Telefone))
+            .ForMember(dest => dest.Tipo, opt => opt.MapFrom(src => src.IdPessoaNavigation.Tipo))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.IdPessoaNavigation.Status))
+            .ForMember(dest => dest.Logradouro, opt => opt.MapFrom(src => src.IdPessoaNavigation.Logradouro))
+            .ForMember(dest => dest.Numero, opt => opt.MapFrom(src => src.IdPessoaNavigation.Numero))
+            .ForMember(dest => dest.Complemento, opt => opt.MapFrom(src => src.IdPessoaNavigation.Complemento))
+            .ForMember(dest => dest.Bairro, opt => opt.MapFrom(src => src.IdPessoaNavigation.Bairro))
+            .ForMember(dest => dest.Cidade, opt => opt.MapFrom(src => src.IdPessoaNavigation.Cidade))
+            .ForMember(dest => dest.Estado, opt => opt.MapFrom(src => src.IdPessoaNavigation.Estado));
+
+        CreateMap<FuncionarioViewModel, Funcionario>();
+
+
     }
 }
