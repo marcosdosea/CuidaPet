7dcf83cc733bee1f39d276b5dbfdc6404ee9c43e
kewensilva-gif <kewensilva58@gmail.com>
2025-09-10 19:35:24 -0300

    add service de agendamento

---
+++ Codigo/Core/Service/IAgendamentoService.cs
@@ -0,0 +1,17 @@
+﻿using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IAgendamentoService
+    {
+        uint Create(Agendamento agendamento);
+        void Edit(Agendamento agendamento);
+        void Delete(uint id);
+        Agendamento? Get(uint id);
+        IEnumerable<Agendamento> GetAll(int page, int pageSize);
+        IEnumerable<AgendamentoDto> GetByPet(uint idPet);
+        IEnumerable<AgendamentoDto> GetByTutor(uint idTutor);
+        IEnumerable<AgendamentoDto> GetByFuncionario(uint idFuncionario);
+        int GetCount();
+    }
+}
