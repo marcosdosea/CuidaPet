04db6cd3ce0fb9845b66d973305a8642d9afea31
kewensilva-gif <kewen.silva@jetsalesbrasil.com.br>
2025-12-02 21:07:30 -0300

    Feat: Adicionar testes unitários para os controladores Doenca, Especie e Vacina para lidar com casos extremos.

--- Codigo/CuidaPetWebTests/Controllers/DoencaControllerTests.cs
+++ Codigo/CuidaPetWebTests/Controllers/DoencaControllerTests.cs
@@ -135,6 +135,115 @@ namespace CuidaPetWeb.Controllers.Tests
             Assert.AreEqual("Index", redirectToActionResult.ActionName);
         }
 
+        [TestMethod()]
+        public void IndexTest_CondicaoComposta_PaginaPadrao()
+        {
+            var result = controller.Index(1, 10);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(1, viewResult.ViewData["Page"]);
+            Assert.AreEqual(10, viewResult.ViewData["PageSize"]);
+        }
+
+        [TestMethod()]
+        public void IndexTest_CondicaoComposta_PaginaPersonalizada()
+        {
+            var mockService = new Mock<IDoencaService>();
+            mockService.Setup(s => s.GetAll(2, 5)).Returns(GetTestDoencas().Take(2));
+            mockService.Setup(s => s.GetCount()).Returns(3);
+            
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new DoencaProfile())).CreateMapper();
+            var tempController = new DoencaController(mockService.Object, mapper);
+            
+            var result = tempController.Index(2, 5);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(2, viewResult.ViewData["Page"]);
+            Assert.AreEqual(5, viewResult.ViewData["PageSize"]);
+        }
+
+        [TestMethod()]
+        public void IndexTest_CondicaoComposta_PaginaZero()
+        {
+            var result = controller.Index(0, 10);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void IndexTest_CondicaoComposta_PageSizeZero()
+        {
+            var result = controller.Index(1, 0);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void DetailsTest_AdivinhacaoErro_IdInexistente()
+        {
+            var mockService = new Mock<IDoencaService>();
+            mockService.Setup(s => s.Get(999)).Returns((Doenca?)null);
+            
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new DoencaProfile())).CreateMapper();
+            var tempController = new DoencaController(mockService.Object, mapper);
+            
+            var result = tempController.Details(999);
+        }
+
+        [TestMethod()]
+        public void DetailsTest_AdivinhacaoErro_IdZero()
+        {
+            var mockService = new Mock<IDoencaService>();
+            mockService.Setup(s => s.Get(0)).Returns((Doenca?)null);
+            
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new DoencaProfile())).CreateMapper();
+            var tempController = new DoencaController(mockService.Object, mapper);
+            
+            var result = tempController.Details(0);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void DetailsTest_AdivinhacaoErro_IdMaximo()
+        {
+            var mockService = new Mock<IDoencaService>();
+            mockService.Setup(s => s.Get(uint.MaxValue)).Returns((Doenca?)null);
+            
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new DoencaProfile())).CreateMapper();
+            var tempController = new DoencaController(mockService.Object, mapper);
+            
+            var result = tempController.Details(uint.MaxValue);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void EditTest_Get_AdivinhacaoErro_IdInexistente()
+        {
+            var mockService = new Mock<IDoencaService>();
+            mockService.Setup(s => s.Get(999)).Returns((Doenca?)null);
+            
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new DoencaProfile())).CreateMapper();
+            var tempController = new DoencaController(mockService.Object, mapper);
+            
+            var result = tempController.Edit(999);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Get_AdivinhacaoErro_IdInexistente()
+        {
+            var mockService = new Mock<IDoencaService>();
+            mockService.Setup(s => s.Get(999)).Returns((Doenca?)null);
+            
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new DoencaProfile())).CreateMapper();
+            var tempController = new DoencaController(mockService.Object, mapper);
+            
+            var result = tempController.Delete(999);
+        }
+
         private DoencaViewModel GetNewDoenca()
         {
             return new DoencaViewModel()
