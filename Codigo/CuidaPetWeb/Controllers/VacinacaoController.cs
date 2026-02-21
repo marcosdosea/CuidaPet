230443ed5abd51b1fbcd1da5d9741cde8bbb961f
kewensilva-gif <kewen.silva@jetsalesbrasil.com.br>
2026-02-21 19:01:47 -0300

    feat: enhance VacinacaoController to include additional services and populate view bags for dropdowns

--- Codigo/CuidaPetWeb/Controllers/VacinacaoController.cs
+++ Codigo/CuidaPetWeb/Controllers/VacinacaoController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using Core;
 using Core.Service;
 using CuidaPetWeb.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CuidaPetWeb.Controllers
 {
@@ -10,12 +12,36 @@ namespace CuidaPetWeb.Controllers
     public class VacinacaoController : Controller
     {
         private readonly IVacinacaoService vacinacaoService;
+        private readonly IPetService petService;
+        private readonly IVacinaService vacinaService;
+        private readonly IFuncionarioService funcionarioService;
+        private readonly IPessoaService tutorService;
         private readonly IMapper mapper;
 
-        public VacinacaoController(IVacinacaoService vacinacaoService, IMapper mapper)
+        public VacinacaoController(IVacinacaoService vacinacaoService, IPetService petService, IVacinaService vacinaService, IFuncionarioService funcionarioService, IPessoaService tutorService, IMapper mapper)
         {
             this.vacinacaoService = vacinacaoService;
+            this.petService = petService;
+            this.vacinaService = vacinaService;
+            this.funcionarioService = funcionarioService;
+            this.tutorService = tutorService;
             this.mapper = mapper;
+        }
+
+        private void PopularViewBags()
+        {
+            int page = 1;
+            int pageSize = 20;
+            
+            var listaDePets = petService.GetAll(page, pageSize) ?? new List<Pet>();
+            var listaDeVacinas = vacinaService.GetAll(page, pageSize) ?? new List<Vacina>();
+            var listaDeFuncionarios = funcionarioService.GetAll(page, pageSize) ?? new List<Funcionario>();
+            var listaDeTutores = tutorService.GetTutores() ?? new List<Pessoa>();
+            
+            ViewBag.Pets = new SelectList(listaDePets, "Id", "Nome");
+            ViewBag.Vacinas = new SelectList(listaDeVacinas, "Id", "Nome");
+            ViewBag.Funcionarios = new SelectList(listaDeFuncionarios, "Id", "Nome");
+            ViewBag.Tutores = new SelectList(listaDeTutores, "Id", "Nome");
         }
 
         public ActionResult Index(int page = 1, int pageSize = 10)
@@ -36,19 +62,21 @@ namespace CuidaPetWeb.Controllers
             if (vacinacao == null)
                 return NotFound();
 
+            PopularViewBags();
+
             var vacinacaoViewModel = mapper.Map<VacinacaoViewModel>(vacinacao);
             return View(vacinacaoViewModel);
         }
 
         public ActionResult Create()
         {
-            // Carregar listas para selects se necessário
+            PopularViewBags();
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([FromBody] VacinacaoViewModel vacinacaoViewModel)
+        public ActionResult Create(VacinacaoViewModel vacinacaoViewModel)
         {
             if (ModelState.IsValid)
             {
@@ -56,6 +84,8 @@ namespace CuidaPetWeb.Controllers
                 vacinacaoService.Create(vacinacao);
                 return RedirectToAction(nameof(Index));
             }
+            
+            PopularViewBags();
             return View(vacinacaoViewModel);
         }
 
@@ -65,6 +95,7 @@ namespace CuidaPetWeb.Controllers
             if (vacinacao == null)
                 return NotFound();
 
+            PopularViewBags();
             var vacinacaoViewModel = mapper.Map<VacinacaoViewModel>(vacinacao);
             return View(vacinacaoViewModel);
         }
@@ -79,6 +110,8 @@ namespace CuidaPetWeb.Controllers
                 vacinacaoService.Edit(vacinacao);
                 return RedirectToAction(nameof(Index));
             }
+            
+            PopularViewBags();
             return View(vacinacaoViewModel);
         }
 
